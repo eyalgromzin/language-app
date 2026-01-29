98140c89ee375aede4576ed657a4e746e670d431
eyalgromzin <eyalgromzin@gmail.com>
2026-01-29 10:46:31 +0200

    make app ltr

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
  */
 
 import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, I18nManager } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import installNetworkLogger from './src/utils/networkInterceptor';
+
+// Force LTR layout at the earliest point
+I18nManager.allowRTL(false);
+I18nManager.forceRTL(false);
 
 // Install network logger as early as possible
 installNetworkLogger({
